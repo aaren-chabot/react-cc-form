301a2565e212ead768f5b4878d1a70c2aa135039
Aaren Chabot <chabot.aaren@gmail.com>
2020-05-11 17:20:05 -0400

    cardBack styles

--- src/components/Card/CardBack/CardBack.js
+++ src/components/Card/CardBack/CardBack.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 import styles from './CardBack.module.scss';
+import cardImg from '../../../assets/visa.png';
 
 const CardBack = () => {
-  return <div className={styles.back}></div>;
+  const { back, band, cvv, logo } = styles;
+  return (
+    <div className={back}>
+      <div className={band}></div>
+      <div className={cvv}>
+        <span>CVV</span>
+        <div></div>
+      </div>
+      <div className={logo}>
+        <img src={cardImg} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default CardBack;
